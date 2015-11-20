daf73485b880c6b3372ba996feef3428bede69c9
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-20 18:40:33 -0200

    Alicerces Rails - parte 2 - Concluído

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151120201340) do
+
+  create_table "opinions", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
